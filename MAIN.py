988694cc92fc412de5d56825e4a86fadc39c45c4
ashfu96 <119931578+ashfu96@users.noreply.github.com>
2023-05-22 18:45:09 +0100

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -64,8 +64,8 @@ if test_data_file is not None:
     
     # EXPANDER DATASET ORGINALE
     expander = st.expander("Vedi dataset originale")
-    #original = myfunction.load_data(test_data_file)
-    expander.write(df_test.head(200))
+    original = myfunction.load_data(test_data_file)
+    expander.write(original.head(13097))
    
 #################################################################################    
 #           SIDEBAR E INFO DOPO LA SELEZIONE UNITà
