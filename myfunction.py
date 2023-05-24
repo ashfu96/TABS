f82957768796ac1baa39dcbc604ef84e2b93553e
ashfu96 <119931578+ashfu96@users.noreply.github.com>
2023-05-24 10:51:36 +0100

    Update myfunction.py

--- myfunction.py
+++ myfunction.py
@@ -8,6 +8,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
+from keras.preprocessing import sequence
+
 ########################################################
       ######## CARICAMENTO E PREPROCESING ########
 ########################################################
