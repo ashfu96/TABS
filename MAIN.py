dd7cd983a984a90fdaeb3c81989f074fcdf4246f
ashfu96 <119931578+ashfu96@users.noreply.github.com>
2023-05-24 12:06:47 +0100

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -254,18 +254,18 @@ if test_data_file is not None:
 ################################################################
 # TAB 2
 
-not_null2 = result_df2[result_df2['prediction'].notnull()].copy()
-not_null2["prediction"] = not_null2["prediction"].astype(int)
+    not_null2 = result_df2[result_df2['prediction'].notnull()].copy()
+    not_null2["prediction"] = not_null2["prediction"].astype(int)
 
-subset_df2 = result_df2[result_df2['prediction'].notnull()].copy()
-subset_df2["prediction"] = subset_df2["prediction"].astype(int)
+    subset_df2 = result_df2[result_df2['prediction'].notnull()].copy()
+    subset_df2["prediction"] = subset_df2["prediction"].astype(int)
 
-null2 = result_df2[result_df2['prediction'].isnull()].copy()
-null2["prediction"] = null2["prediction"].fillna('In control')
+    null2 = result_df2[result_df2['prediction'].isnull()].copy()
+    null2["prediction"] = null2["prediction"].fillna('In control')
 
-subset_df_part_1 = subset_df2[subset_df2['prediction'] < 10].copy()
-subset_df_part_2 = subset_df2[(subset_df2['prediction'] >= 10) & (subset_df2['prediction'] <= 25)].copy()
-subset_df_part_3 = subset_df2[subset_df2['prediction'] > 25].copy()
+    subset_df_part_1 = subset_df2[subset_df2['prediction'] < 10].copy()
+    subset_df_part_2 = subset_df2[(subset_df2['prediction'] >= 10) & (subset_df2['prediction'] <= 25)].copy()
+    subset_df_part_3 = subset_df2[subset_df2['prediction'] > 25].copy()
     
     prova1, prova2, prova3  = st.tabs(["less_than_10", "between_11_25", "greater_than_25"])
 
