b43141083b29d84eb3dcacad466ec03f72b063bd
ashfu96 <119931578+ashfu96@users.noreply.github.com>
2023-05-26 18:16:41 +0100

    Update myfunction.py

--- myfunction.py
+++ myfunction.py
@@ -246,9 +246,9 @@ def get_last_sequences_with_predictions(df, sequence_cols, sequence_length, mode
 @st.cache
 def convert_df(df):
     
-    df_copia = df.copy()
+    #df_copia = df.copy()
     # Effettua il casting a intero dell'ultima colonna del dataframe
-    df_copia.iloc[:, -1] = df_copia.iloc[:, -1].astype(int)  
+    #df_copia.iloc[:, -1] = df_copia.iloc[:, -1].astype(int)  
       
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df_copia.to_csv().encode('utf-8')
